5266bdefb159c93052c5f83857aa28409677031f
Sergiu Postolachi <sergiu.postolachi@endava.com>
2017-11-03 11:21:10 +0200

    Add gitignore file

--- src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
+++ src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
@@ -1,37 +1,38 @@
 package com.javafortesters.chap003myfirsttest.examples;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MyFirstTest {
 
     @Test
     public void canAddTwoPlusTwo() {
-        int answer = 2+2;
+        int answer = 2 + 2;
         //assertThat();
-        assertEquals("2+2=4", 4, answer );
+        assertEquals("2+2=4", 4, answer);
     }
 
- @Test
+    @Test
     public void operationExOne() {
-        int answer = 2-2;
+        int answer = 2 - 2;
         //assertThat();
-        assertEquals("2-2=0", 0, answer );
+        assertEquals("2-2=0", 0, answer);
     }
 
 
     @Test
     public void operationExTwo() {
-        int answer = 4/2;
+        int answer = 4 / 2;
         //assertThat();
-        assertEquals("4/2=2", 2, answer );
+        assertEquals("4/2=2", 2, answer);
     }
 
     @Test
     public void operationExThree() {
-        int answer = 2*2;
+        int answer = 2 * 2;
         //assertThat();
-        assertEquals("2*2=4", 4, answer );
+        assertEquals("2*2=4", 4, answer);
     }
 }
 
