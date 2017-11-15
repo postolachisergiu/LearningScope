26050b1c8e85bf2836ac7c7ecae28cb4cc0f2d35
Sergiu Postolachi <sergiu.postolachi@endava.com>
2017-11-15 16:45:37 +0200

    convert to hex, working with Integer

--- src/test/java/com/javafortesters/chap004testswithotherclasses/examples/IntegerExamplesTest.java
+++ src/test/java/com/javafortesters/chap004testswithotherclasses/examples/IntegerExamplesTest.java
@@ -1,11 +1,28 @@
 package com.javafortesters.chap004testswithotherclasses.examples;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class IntegerExamplesTest {
 
     @Test
     public void integerExploration() {
 
+    }
+
+    @Test
+    public void toHexString(){
+        System.out.println(convertIntToHex(10));
+    }
+
+    @Test
+    public void maxMinCheck(){
+        assertEquals(-2147483648, Integer.MIN_VALUE);
+        assertEquals(2147483647, Integer.MAX_VALUE);
+
+    }
+
+    public String convertIntToHex(int x){
+        return Integer.toHexString(x);
     }
 }
