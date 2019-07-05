a1906a301caa09add815265d91e6a489581511ae
spostolachi <postolachi.sergiu@gmail.com>
2019-07-05 10:54:26 +0300

    oca start point

---
+++ src/main/java/com/oca/library/Librarian.java
@@ -0,0 +1,10 @@
+package com.oca.library;
+
+public class Librarian {
+
+    public Librarian(){
+        Book book = new Book();
+        book.author = "Test";
+        book.modifyTemplate();
+    }
+}
