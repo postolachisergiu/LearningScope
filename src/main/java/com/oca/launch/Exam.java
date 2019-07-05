e1c3e650ae23989bd9f6e1a8d78cc272dfc04f9b
spostolachi <postolachi.sergiu@gmail.com>
2019-07-05 11:55:37 +0300

    jdk 12

--- src/main/java/com/oca/launch/Exam.java
+++ src/main/java/com/oca/launch/Exam.java
@@ -1,13 +1,10 @@
 package com.oca.launch;
 
-import com.oca.library.CourseBook;
 
 public class Exam {
 
     public static void main (String [] args)
     {
         System.out.println("Good luck!");
-        CourseBook cb = new CourseBook();
-        System.out.println(cb);
     }
 }
